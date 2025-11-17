06690c27fc5669675f151e9af858e91f055317a2
Kasper <polinalevcenko55@gmail.com>
2025-11-17 21:41:41 +0700

    add base application

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from .models import CustomerUser, Application
+from django.core.paginator import Paginator
+from .models import Application
 from .forms import RegistrationForm, ApplicationForm
-
 def index(request):
-    done_applications = Application.objects.filter(status='done').order_by('-created_at')[:4]
-    print("Количество выполненных заявок:", done_applications.count())  # ← Для отладки
-    for app in done_applications:
-        print(f"ID: {app.id}, Title: {app.title}, Status: {app.status}")
+    done_applications = Application.objects.filter(status='done').order_by('-created_at')
+
+    # Пагинация
+    paginator = Paginator(done_applications, 4)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'done_applications': done_applications
+        'done_applications': page_obj, 
+        'is_paginated': page_obj.has_other_pages(), 
+        'page_obj': page_obj, 
     }
     return render(request, 'index.html', context)
 
@@ -33,7 +37,6 @@ def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
-            # Сохраняем пользователя
             user = form.save()
             login(request, user)
             messages.success(request, "Регистрация прошла успешно! Вы вошли в систему.")
@@ -55,7 +58,7 @@ def appliation_views(request):
         if form.is_valid():
             application = form.save()
             messages.success(request, 'Заявка успешно создана!')
-            return redirect('application_success')  # замените на ваш URL
+            return redirect('application_success') 
         else:
             messages.error(request, 'Пожалуйста исправте ошибки')
     else:
