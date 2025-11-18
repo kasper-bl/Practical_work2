de072a718937b26d90ff3ef3343a6c3d3c9973f1
Kasper <polinalevcenko55@gmail.com>
2025-11-18 09:57:35 +0700

    removing unnecessary

--- Design_pro/urls.py
+++ Design_pro/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)), 
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
